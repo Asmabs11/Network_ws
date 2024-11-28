1e5ba47f21de38552de3512bb2047f6c44ce975b
asma ben salah <asmabensalah468@gmail.com>
2024-11-28 15:48:23 +0100

    Weather

--- script.js
+++ script.js
@@ -10,16 +10,31 @@ btn.addEventListener('click', ()=> {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=38ecfa91763543ecbb95222766aa5bc6&units=metric`
 
-    axios.get(url)
-.then((res)=>{
-    console.log(res.data)
-    temp.innerHTML = res.data.main.temp + " °C"
-    main.innerHTML = res.data.weather[0].main
-    description.innerHTML = res.data.weather[0].description
-    city.innerHTML = res.data.name
-    country.innerHTML = res.data.sys.country
+//     axios.get(url)
+// .then((res)=>{
+//     console.log(res.data)
+//     temp.innerHTML = res.data.main.temp + " °C"
+//     main.innerHTML = res.data.weather[0].main
+//     description.innerHTML = res.data.weather[0].description
+//     city.innerHTML = res.data.name
+//     country.innerHTML = res.data.sys.country
 
+// })
+// .catch((err)=> console.log(err))
+
+fetch(url)
+.then((response)=> response.json())
+.then((data)=> {
+    temp.innerHTML = data.main.temp + " °C"
+    main.innerHTML = data.weather[0].description
+    description.innerHTML = data.weather[0].description
+    city.innerHTML = data.name
+    country.innerHTML = data.sys.country
 })
-.catch((err)=> console.log(err))
+
+
+
+
+
 })
 
